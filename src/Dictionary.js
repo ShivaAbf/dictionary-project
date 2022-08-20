286590caa21ffed3a2e39026f3a08de1089bd498
ShivaAbf <88429496+ShivaAbf@users.noreply.github.com>
2022-08-20 18:11:51 +0430

    shecodes logo

---
+++ src/Dictionary.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Dictionary.css"
+export default function Dictionary(){
+    return(
+        <header><img src="https://www.dictionary.shecodes.io/static/media/logo.951ad035.png" alt="shecodeslogo" className="img-fluid shecodeslogo"/></header>
+     
+    )
+}
