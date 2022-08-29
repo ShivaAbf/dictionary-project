7145b48ab01c8b6709ff9c7c663624644dfa98d1
ShivaAbf <88429496+ShivaAbf@users.noreply.github.com>
2022-08-29 22:31:24 +0430

    unnecessary codes deleted

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
        <div className='row'>
       <header><img src="https://www.dictionary.shecodes.io/static/media/logo.951ad035.png" alt="shecodeslogo" className="img-fluid shecodeslogo"/></header>
       <div className='col-md-9 dictionary-part '>
-<Dictionary defaultWord="sunrise"/>
+<Dictionary defaultWord="friend"/>
 </div>
-<footer><span><p>This page is coded by<a href="https://fancy-pastelito-ab221e.netlify.app/about.html">Shiva Aboufazel</a>and it's open source on <a href='https://github.com/ShivaAbf/dictionary-project'>github</a> and hosted on <a href='https://benevolent-basbousa-543c21.netlify.app/'>netlify</a></p></span></footer>
+<footer><span><p>This page is coded by <a href="https://fancy-pastelito-ab221e.netlify.app/about.html">Shiva Aboufazel</a> and it's open source on <a href='https://github.com/ShivaAbf/dictionary-project'>github</a> and hosted on <a href='https://benevolent-basbousa-543c21.netlify.app/'>netlify</a></p></span></footer>
 </div>
 </div>
   );
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,35 +2,65 @@ import React, { useState } from "react";
 import "./Dictionary.css"
 import axios from "axios";
 import Responses from "./Responses";
+import Images from "./Images";
 
-export default function Dictionary(){
-    let [word , setWord] = useState("");
-    let [results , setResults] = useState(null)
+export default function Dictionary(props){
+    let [word , setWord] = useState(props.defaultWord);
+    let [results , setResults] = useState(null);
+    let [loeded , setLoeded] = useState(false);
+    let [image , setImage] = useState(null);
+
 
     function handleResponse(response){
+        setLoeded(true)
       setResults(response.data[0]);
+    }
+
+    function handlePhotoResponse(response){
+        setImage(response.data.photos)
+    }
+
+    function search(){
+        let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+        axios.get(url).then(handleResponse)
+
+        const apikey = `563492ad6f91700001000001b4f4dfa9efed4134a6e176e6d9388a76`
+        let photoUrl =  `https://api.pexels.com/v1/search?query=${word}&per_page=9`
+        axios.get(photoUrl , {
+            headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": apikey
+            }}).then(handlePhotoResponse)
     }
 
     function handleSubmit(event){
         event.preventDefault();
-        let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
-        axios.get(url).then(handleResponse)
+        search();
     }
 
     function handleChange(event){
         setWord(event.target.value);
     }
+    if(loeded){
 
     return(
    <div>
+    <section className="form-section">
+        <h2 className="mb-3">What word are you looking for?</h2>
         <form className="mt-4" onSubmit={handleSubmit}>
-            <input type='search' className="form-control" autoFocus placeholder="Enter a word..." onChange={handleChange}/>
+            <input type='search' className="form-control text-muted" autoFocus placeholder="Enter a word..." onChange={handleChange}/>
         </form>
+        </section>
         <div>
-            <Responses results={results} />  
+        <Responses results={results} />  
+        <Images photos={image} />
         </div>
          
         </div>
     )
+}
+else 
+search();
    
 }
